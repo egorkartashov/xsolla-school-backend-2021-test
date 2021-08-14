e93c74a5e2007692384c7b2143c0992cd71d2c58
Egor Kartashov <egor.linkinked@gmail.com>
2021-08-14 13:27:49 +0500

    Подцепил graphql и начал писать схему

---
+++ api/graphqlSchema.go
@@ -0,0 +1,26 @@
+package api
+
+import "github.com/graphql-go/graphql"
+
+var productType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Product",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"sku": &graphql.Field{
+				Type: graphql.String,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+			"type": &graphql.Field{
+				Type: graphql.String,
+			},
+			"priceInCents": &graphql.Field{
+				Type: graphql.Int,
+			},
+		},
+	},
+)
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.8.0
+	github.com/graphql-go/graphql v0.7.9 // indirect
 	github.com/jackc/pgproto3/v2 v2.1.0 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/leodido/go-urn v1.2.1 // indirect
