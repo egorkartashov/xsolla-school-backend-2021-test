1689ebdd43e896237ade4e570cac04424cb7869f
Egor Kartashov <egor.linkinked@gmail.com>
2021-08-15 12:03:04 +0500

    Дооткатил лишнее изменение

--- controllers/productsController.go
+++ controllers/productsController.go
@@ -73,7 +73,7 @@ func (controller *ProductsController) GetProducts(writer http.ResponseWriter, re
 
 	productsFilters := filters.BuildProductsFilters(productType, minPrice, maxPrice)
 
-	products, requestResult := controller.productsService.GetProducts(&productsFilters, offset, limit)
+	products, requestResult := controller.productsService.GetProducts(productsFilters, offset, limit)
 	if requestResult.Status == services.Success {
 		size := len(products)
 		pagination := dto.CreatePaginationWithLinks(request.URL.Path, offset, limit, size)
