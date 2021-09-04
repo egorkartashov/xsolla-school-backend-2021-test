3b373c1d10db59e841c43568f73116e697f6a2df
Egor Kartashov <egor.linkinked@gmail.com>
2021-09-04 12:36:54 +0500

    Add fake test for CI pipeline

---
+++ tests/funtional_tests.go
@@ -0,0 +1,11 @@
+package tests
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+//goland:noinspection GoUnusedExportedFunction
+func TestFake(t *testing.T) {
+	assert.True(t, true)
+}
